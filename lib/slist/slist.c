8baa32002430f2abd2e5d89702d7a35d02ecf947
Eduardo Rolim <vndmtrx@gmail.com>
2015-06-10 12:07:35 -0300

    Change slist_init name to slist_create. Change "pointer to list" to "pointer to pointer to list". Add slist_create implementation.

--- lib/slist/slist.c
+++ lib/slist/slist.c
@@ -1,13 +1,19 @@
 #include "slist.h"
 
-struct slist_root {
+struct _slist_root {
 	int size;			// Size of the list structure.
 	slist_node *head;	// Position from where we start walking the list.
 }
 
-struct slist_node {
+struct _slist_node {
 	slist_node *next;	// Pointer to next slist_node element.
 	slist_root *root;	// Pointer to initial list structure.
 	void *item;			// Pointer to the element added on the list.
 }
 
+void slist_create(slist_root **list) {
+	slist_root newlist = (slist_root *) malloc(sizeof(slist_root);
+	newlist->size = 0;
+	newlist->head = NULL;
+	*list = &newlist;
+}
--- lib/slist/slist.h
+++ lib/slist/slist.h
@@ -1,13 +1,13 @@
 #ifndef _LIBADT_SLIST_H
 #define _LIBADT_SLIST_H
 
-typedef struct slist_root slist_root;
+typedef struct _slist_root slist_root;
 
-typedef struct slist_node slist_node;
+typedef struct _slist_node slist_node;
 
-void slist_init(slist_root *list);
+void slist_create(slist_root **list);
 
-void slist_destroy(slist_root *list);
+void slist_destroy(slist_root **list);
 
 int slist_insert_el(slist_root *list, slist_node *current, const void *data);
 
@@ -18,6 +18,8 @@ int slist_size(slist_root *list);
 int slist_head(slist_root *list);
 
 int slist_tail(slist_root *list);
+
+#define slist_empty(list) ((list)->size == 0))
 
 #define slist_el_is_head(list, element) ((((element)->root == (list)) && ((element) == (list)->head)) ? 1 : 0)
 
