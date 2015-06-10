d0b139a0958fec4465660fe4cf8df008504270ec
Eduardo Rolim <vndmtrx@gmail.com>
2015-06-10 09:53:52 -0300

    Add function headers for basic single list operations.
    Change root and node structs to reflect the new function headers contract.

--- lib/slist/slist.c
+++ lib/slist/slist.c
@@ -1,11 +1,13 @@
 #include "slist.h"
 
 struct slist_root {
-	slist_node *first;
+	int size;			// Size of the list structure.
+	slist_node *head;	// Position from where we start walking the list.
 }
 
 struct slist_node {
-	slist_node *next;
-	void *item;
+	slist_node *next;	// Pointer to next slist_node element.
+	slist_root *root;	// Pointer to initial list structure.
+	void *item;			// Pointer to the element added on the list.
 }
 
--- lib/slist/slist.h
+++ lib/slist/slist.h
@@ -5,4 +5,26 @@ typedef struct slist_root slist_root;
 
 typedef struct slist_node slist_node;
 
+void slist_init(slist_root *list);
+
+void slist_destroy(slist_root *list);
+
+int slist_insert_next(slist_root *list, slist_node *current, const void *data);
+
+int slist_rem_next(slist_root *list, slist_node *current, void **data);
+
+int slist_size(slist_root *list);
+
+int slist_head(slist_root *list);
+
+int slist_tail(slist_root *list);
+
+#define slist_el_is_head(list, element) ((((element)->root == (list)) && ((element) == (list)->head)) ? 1 : 0)
+
+#define slist_el_is_tail(list, element) ((((element)->root == (list)) && ((element)->next == NULL)) ? 1 : 0)
+
+#define slist_data(element) ((element)->data)
+
+#define slist_next(element) ((element)->next)
+
 #endif
