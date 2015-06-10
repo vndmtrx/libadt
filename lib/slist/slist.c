db598410234d74ed9565e824064de032fa1c656a
Eduardo Rolim <vndmtrx@gmail.com>
2015-06-10 16:57:32 -0300

    Add documentation and slist_insert_el and change struct _slist_root.
    - Add documentation on header and implementation files;
    - Add pointer to function responsible for destroy data when slist_destroy be called to free structure memory;
    - Add function slist_insert_el to insert elements on the list;
    - Add includes to deal with NULL and perror;
    - Remove slist_empty;
    - Rename slist_data to slist_el_data and slist_next to slist_el_next.

--- lib/slist/slist.c
+++ lib/slist/slist.c
@@ -1,7 +1,11 @@
 #include "slist.h"
 
+#include <stdio.h>
+#include <stdlib.h>
+
 struct _slist_root {
 	int size;			// Size of the list structure.
+	void (*destroyfunc)(void *data);
 	slist_node *head;	// Position from where we start walking the list.
 }
 
@@ -11,9 +15,56 @@ struct _slist_node {
 	void *item;			// Pointer to the element added on the list.
 }
 
-void slist_create(slist_root **list) {
+/* 
+ * Create a empty list structure, set a destroy function for its elements
+ * and return the new structure on first function parameter.
+ * The destroy argument gives a way to free the entire structure when we
+ * call slist_destroy. For malloc/calloc data, free must be used. If data
+ * is a struct with other members, a function designed to free its memory
+ * can be provided. If the data is static or have another way to free its
+ * memory outside, NULL must be set.
+ * Complexity: O(1).
+ */
+void slist_create(slist_root **list, void (destroy)(void *data)) {
 	slist_root newlist = (slist_root *) malloc(sizeof(slist_root);
 	newlist->size = 0;
 	newlist->head = NULL;
+	newlist->destroyfunc = destroyfunc;
 	*list = &newlist;
+}
+
+/*
+ * Insert an element in the list after the current element indicated.
+ * If *current is NULL, *data is appended on the head.
+ * Complexity: O(1).
+ */
+int slist_insert_el(slist_root *list, slist_node *current, const void *data) {
+	slist_node *newelement;
+	newelement = (slist_node *) malloc(sizeof(slist_node));
+	if (newelement == NULL) {
+		perror("Can't create new element.");
+		return -1;
+	}
+	newelement->item = data;
+	newelement->root = list;
+	newelement->next = NULL;
+	if (current == NULL) {
+		if (list->head != NULL) {
+			newelement->next = list->head;
+		}
+		list->head = newelement;
+	} else {
+		if (current->root == list) {
+			if (current->next != NULL) {
+				newelement->next = slist_el_next(current);
+			}
+			current->next = newelement;
+		} else {
+			free(newelement);
+			perror("The 'current' element doesn't belong to 'list'.");
+			return -2;
+		}
+	}
+	list->size++;
+	return 0;
 }
--- lib/slist/slist.h
+++ lib/slist/slist.h
@@ -5,28 +5,72 @@ typedef struct _slist_root slist_root;
 
 typedef struct _slist_node slist_node;
 
-void slist_create(slist_root **list);
 
-void slist_destroy(slist_root **list);
+/* 
+ * Create a empty list structure, set a destroy function for its elements
+ * and return the new structure on first function parameter.
+ * The destroy argument gives a way to free the entire structure when we
+ * call slist_destroy. For malloc/calloc data, free must be used. If data
+ * is a struct with other members, a function designed to free its memory
+ * must be provided. If the data is static or have another way to free its
+ * memory, NULL must be set.
+ * Complexity: O(1).
+ */
+void slist_create(slist_root **list, void (destroy)(void *data));
 
+
+/*
+ * Insert an element in the list after the current element indicated.
+ * If *current is NULL, *data is not appended.
+ * Complexity: O(1).
+ */
 int slist_insert_el(slist_root *list, slist_node *current, const void *data);
 
+/*
+ * Remove the element indicated by *current and save the respective 
+ * data in **data.
+ */
 int slist_rem_el(slist_root *list, slist_node *current, void **data);
 
+/*
+ * Returns the size of the list.
+ */
 int slist_size(slist_root *list);
 
+/*
+ *  Returns the first element of the list.
+ */
 int slist_head(slist_root *list);
 
+/*
+ * Returns the last element of the list.
+ */
 int slist_tail(slist_root *list);
 
-#define slist_empty(list) ((list)->size == 0))
+/*
+ * Destroy the list and the elements in it. If destroy function is provided,
+ * it will be used, otherwise, uses free.
+ */
+void slist_destroy(slist_root **list);
 
+/*
+ * Test if element is actually the head of the list.
+ */
 #define slist_el_is_head(list, element) ((((element)->root == (list)) && ((element) == (list)->head)) ? 1 : 0)
 
+/*
+ * Test if element is actually the tail of the list.
+ */
 #define slist_el_is_tail(list, element) ((((element)->root == (list)) && ((element)->next == NULL)) ? 1 : 0)
 
-#define slist_data(element) ((element)->data)
+/*
+ * Return the data from the element.
+ */
+#define slist_el_data(element) ((element)->data)
 
-#define slist_next(element) ((element)->next)
+/*
+ * Return the next element on the list.
+ */
+#define slist_el_next(element) ((element)->next)
 
 #endif
