ebf9b8c6a0f7538e7762c650b79edbcc44fab32b
Eduardo Rolim <vndmtrx@gmail.com>
2015-06-26 16:38:49 -0300

    Implement sl_queue functions.
    Add sl_queue_push;
    Add sl_queue_peek;
    Add sl_queue pop;

--- lib/queues/sl_queue.c
+++ lib/queues/sl_queue.c
@@ -7,6 +7,29 @@ void sl_queue_create(sl_queue_root *queue, t_destroyfunc destroyfunc) {
 	slist_create(queue, destroyfunc);
 }
 
+int sl_queue_push(sl_queue_root *queue, void *data) {
+	return slist_insert_el_next(queue, queue->tail, data);
+}
+
+int sl_queue_peek(sl_queue_root *queue, void **data) {
+	if (slist_size(queue) > 0) {
+		*data = queue->head->data;
+		return 0;
+	} else {
+		perror("Queue is empty!");
+		return -1;
+	}
+}
+
+int sl_queue_pop(sl_queue_root *queue, void **data) {
+	if (slist_size(queue) > 0) {
+		return slist_rem_el(queue, queue->head, data);
+	} else {
+		perror("Queue is empty!");
+		return -1;
+	}
+}
+
 void sl_queue_destroy(sl_queue_root *queue) {
 	slist_destroy(queue);
 }
