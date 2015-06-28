ae21185d254e001e5875bbb6d32651a3d30bc60c
Eduardo Rolim <vndmtrx@gmail.com>
2015-06-28 11:23:40 -0300

    Fix queue comments on sl_queue.h.

--- includes/sl_queue.h
+++ includes/sl_queue.h
@@ -6,7 +6,9 @@
 typedef slist_root sl_queue_root;
 
 /* 
- * Create a empty queue structure and set a destroy function for its elements.
+ * Create a empty queue structure (FiFo - Fist In / First Out) and set a
+ * destroy function for its elements.
+ * 
  * The destroy argument gives a way to free the entire structure when we
  * call slist_destroy. For malloc/calloc data, free must be used. If data
  * is a struct with other members, a function designed to free its memory
@@ -37,7 +39,7 @@ int sl_queue_peek(sl_queue_root *queue, void **data);
 int sl_queue_pop(sl_queue_root *queue, void **data);
 
 /*
- * Destroy the list and the elements in it. If destroy function is provided,
+ * Destroy the queue and the elements in it. If destroy function is provided,
  * it will be used, otherwise, uses free.
  * Complexity: O(n).
  */
