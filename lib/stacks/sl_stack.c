8a96d6d8bb5f9a05c92ebb9a83a621390bee68b7
Eduardo Rolim <vndmtrx@gmail.com>
2015-06-28 11:29:04 -0300

    Add sl_stack implementation.

---
+++ lib/stacks/sl_stack.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sl_stack.h>
+
+/* 
+ * Create a empty stack structure (FiLo - First In / Last Out) and set a
+ * destroy function for its elements.
+ * 
+ * The destroy argument gives a way to free the entire structure when we
+ * call slist_destroy. For malloc/calloc data, free must be used. If data
+ * is a struct with other members, a function designed to free its memory
+ * must be provided. If the data is static or have another way to free its
+ * memory, NULL must be set.
+ * Complexity: O(1).
+ */
+void sl_stack_create(sl_stack_root *stack, t_destroyfunc destroyfunc) {
+	slist_create(stack, destroyfunc);
+}
+
+/*
+ * Insert an element in the stack.
+ * Complexity: O(1).
+ */
+int sl_stack_push(sl_stack_root *stack, void *data) {
+	return slist_insert_el_next(stack, NULL, data);
+}
+
+/*
+ * Look the last item of the stack without remove it.
+ * Complexity: O(1).
+ */
+int sl_stack_peek(sl_stack_root *stack, void **data) {
+	if (slist_size(stack) > 0) {
+		*data = stack->head->data;
+		return 0;
+	} else {
+		perror("Stack is empty!");
+		return -1;
+	}
+}
+
+/*
+ * Remove an element from the stack and return its element.
+ * Stacks are First In Last Out, so the item removed is the last item
+ * added.
+ * Complexity: O(1).
+ */
+int sl_stack_pop(sl_stack_root *stack, void **data) {
+	if (slist_size(stack) > 0) {
+		return slist_rem_el(stack, stack->head, data);
+	} else {
+		perror("Stack is empty!");
+		return -1;
+	}
+}
+
+/*
+ * Destroy the stack and the elements in it. If destroy function is provided,
+ * it will be used, otherwise, uses free.
+ * Complexity: O(n).
+ */
+void sl_stack_destroy(sl_stack_root *stack) {
+	slist_destroy(stack);
+}
