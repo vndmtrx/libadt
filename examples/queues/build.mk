d7efa2e4d400b641ee4584cb3358fcb5f7d1c38a
Eduardo Rolim <vndmtrx@gmail.com>
2015-06-28 14:51:29 -0300

    Add test target for evaluator.

--- examples/queues/build.mk
+++ examples/queues/build.mk
@@ -1,7 +1,5 @@
 ## Build target for Queue tests
 
-queue_tests: sl_queue_tests
-
 # Queues
 build/evaluator.o: examples/queues/evaluator.c | build
 	$(CC) $(CFLAGS) -c $^ -o $@
@@ -10,3 +8,7 @@ build/evaluator: examples/queues/evaluator.o build/sl_queue.o
 	$(CC) $(LDFLAGS) $^ -o $@
 
 evaluator: build/evaluator
+
+test_evaluator: evaluator
+	echo "123+45+98534" | ./build/$^
+	echo "123+45-STRING&8+98534" | ./build/$^
