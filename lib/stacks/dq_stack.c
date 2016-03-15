8336cb0f11adc11f3ed2088eaa8c06ffd40d5346
Eduardo Rolim <eduardo.rolim@ifpr.edu.br>
2016-03-15 16:27:55 -0300

    Add dq_stack.c initial code.

--- lib/stacks/dq_stack.c
+++ lib/stacks/dq_stack.c
@@ -4,3 +4,27 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
+dq_stack_root * dq_stack_create(t_destroyfunc destroyfunc) {
+	return dl_deque_create(destroyfunc);
+}
+
+int dq_stack_push(dq_stack_root *stack, void *data) {
+	return dl_deque_push_left(stack, data);
+}
+
+void * dq_stack_peek(dq_stack_root *stack) {
+	dl_deque_peek_left(stack);
+}
+
+void * dq_stack_pop(dq_stack_root *stack) {
+	dl_deque_pop_left(stack)
+}
+
+void dq_stack_destroy(dq_stack_root *stack) {
+	dl_deque_destroy(stack);
+}
+
+int dq_stack_size(sl_stack_root *stack) {
+	return dl_deque_size(stack);
+}
