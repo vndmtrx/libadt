72f228710bce4f678c19b30b834a3bd1650724f1
Eduardo Rolim <eduardo.rolim@ifpr.edu.br>
2016-04-01 12:58:30 -0300

    Fix Queue code to meet new list file and function names.

--- lib/queues/sl_queue.c
+++ lib/queues/sl_queue.c
@@ -1,16 +1,16 @@
 #include <sl_queue.h>
 
-#include <slist.h>
+#include <sl_list.h>
 
 #include <stdio.h>
 #include <stdlib.h>
 
 sl_queue_root * sl_queue_create(t_destroyfunc destroyfunc) {
-	return slist_create(destroyfunc);
+	return sl_list_create(destroyfunc);
 }
 
 int sl_queue_push(sl_queue_root *queue, void *data) {
-	return slist_insert_el_next(queue, queue->tail, data);
+	return sl_list_insert_el_next(queue, queue->tail, data);
 }
 
 void * sl_queue_peek(sl_queue_root *queue) {
@@ -24,7 +24,7 @@ void * sl_queue_peek(sl_queue_root *queue) {
 
 void * sl_queue_pop(sl_queue_root *queue) {
 	if (sl_queue_size(queue) > 0) {
-		return slist_rem_el(queue, queue->head);
+		return sl_list_rem_el(queue, queue->head);
 	} else {
 		fprintf(stderr, "Queue is empty!");
 		return NULL;
@@ -32,7 +32,7 @@ void * sl_queue_pop(sl_queue_root *queue) {
 }
 
 void sl_queue_destroy(sl_queue_root *queue) {
-	slist_destroy(queue);
+	sl_list_destroy(queue);
 }
 
 int sl_queue_size(sl_queue_root *queue) {
