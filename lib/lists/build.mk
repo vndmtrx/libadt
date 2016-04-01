6e9cf721574846b901594213d89dcfccddd1e044
Eduardo Rolim <eduardo.rolim@ifpr.edu.br>
2016-04-01 12:51:18 -0300

    Fix Stack code to meet new list file and function names.

--- lib/lists/build.mk
+++ lib/lists/build.mk
@@ -5,10 +5,10 @@ DL_LIST_INC := $(addprefix $(LIB_DIR), lists/dl_list.c) $(addprefix $(INC_DIR), 
 $(addprefix $(BUILD_DIR), sl_list.o): $(SL_LIST_INC) | build
 	$(CC) $(CFLAGS) -c $< -o $@
 
+sl_list.o: $(addprefix $(BUILD_DIR), sl_list.o)
+
 # Build target for Double Linked List
 $(addprefix $(BUILD_DIR), dl_list.o): $(DL_LIST_INC) | build
 	$(CC) $(CFLAGS) -c $< -o $@
-
-sl_list.o: $(addprefix $(BUILD_DIR), sl_list.o)
 
 dl_list.o: $(addprefix $(BUILD_DIR), dl_list.o)
--- lib/stacks/build.mk
+++ lib/stacks/build.mk
@@ -1,8 +1,10 @@
+SL_STACK_INC := $(addprefix $(LIB_DIR), stacks/sl_stack.c) $(addprefix $(INC_DIR), sl_stack.h) $(SL_LIST_INC)
+
 # Build target for singly-list-backed stack
-$(addprefix $(BUILD_DIR), sl_stack.partial): $(addprefix $(LIB_DIR), stacks/sl_stack.c) $(addprefix $(INC_DIR), sl_stack.h slist.h) | build
+$(addprefix $(BUILD_DIR), sl_stack.partial): $(SL_STACK_INC) | build
 	$(CC) $(CFLAGS) -c $< -o $@
 
-$(addprefix $(BUILD_DIR), sl_stack.o): $(addprefix $(BUILD_DIR), sl_stack.partial slist.o)
+$(addprefix $(BUILD_DIR), sl_stack.o): $(addprefix $(BUILD_DIR), sl_stack.partial sl_list.o)
 	ld $(LDFLAGS) -r $^ -o $@
 
 sl_stack.o: $(addprefix $(BUILD_DIR), sl_stack.o)
