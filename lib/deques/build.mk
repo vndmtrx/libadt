0858962a05c60ff01c6e68e7cc462b1721236394
Eduardo Rolim <eduardo.rolim@ifpr.edu.br>
2016-04-01 12:59:23 -0300

    Fix Deque code to meet new list file and function names.

--- lib/deques/build.mk
+++ lib/deques/build.mk
@@ -1,8 +1,10 @@
+DL_DEQUE_INC := $(addprefix $(LIB_DIR), deques/dl_deque.c) $(addprefix $(INC_DIR), dl_deque.h) $(DL_LIST_INC)
+
 # Build target for doubly-list-backed deque
-$(addprefix $(BUILD_DIR), dl_deque.partial): $(addprefix $(LIB_DIR), deques/dl_deque.c) $(addprefix $(INC_DIR), dl_deque.h dlist.h) | build
+$(addprefix $(BUILD_DIR), dl_deque.partial): $(DL_DEQUE_INC) | build
 	$(CC) $(CFLAGS) -c $< -o $@
 
-$(addprefix $(BUILD_DIR), dl_deque.o): $(addprefix $(BUILD_DIR), dl_deque.partial dlist.o)
+$(addprefix $(BUILD_DIR), dl_deque.o): $(addprefix $(BUILD_DIR), dl_deque.partial dl_list.o)
 	ld $(LDFLAGS) -r $^ -o $@
 
 dl_deque.o: $(addprefix $(BUILD_DIR), dl_deque.o)
