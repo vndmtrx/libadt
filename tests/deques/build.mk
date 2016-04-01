238291d94ed69683d645c9f68ddcb13111014694
Eduardo Rolim <eduardo.rolim@ifpr.edu.br>
2016-04-01 15:51:53 -0300

    Add code for build deque tests.

--- tests/deques/build.mk
+++ tests/deques/build.mk
@@ -1,1 +1,19 @@
+## Build target for double-ended queue tests
+
+stack_tests: dl_deque_tests
+
+# Deques
+$(addprefix $(BUILD_DIR), dl_deque_tests.o): $(addprefix $(TESTS_DIR), stacks/dl_deque_tests.c) | build
+	$(CC) $(CFLAGS) -c $^ -o $@
+
+$(addprefix $(BUILD_DIR), dl_deque_tests): $(addprefix $(BUILD_DIR), dl_deque_tests.o dl_deque.o)
+	$(CC) $(LDFLAGS) $^ -o $@
+
+dl_deque_tests: $(addprefix $(BUILD_DIR), dl_deque_tests)
+
+dl_deque_time: dl_deque_tests
+	/usr/bin/time -v $(addprefix $(BUILD_DIR), $^)
+
+dl_deque_vg: dl_deque_tests
+	valgrind -v --leak-check=full $(addprefix $(BUILD_DIR), $^)
 
