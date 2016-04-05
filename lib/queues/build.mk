6903fc4bc438f312879953b130295a17c249a89f
Eduardo Rolim <eduardo.rolim@ifpr.edu.br>
2016-04-05 11:41:59 -0300

    Add build targets for Deque-backed Queues and Stacks.

--- lib/queues/build.mk
+++ lib/queues/build.mk
@@ -1,4 +1,5 @@
 SL_QUEUE_INC := $(addprefix $(LIB_DIR), queues/sl_queue.c) $(addprefix $(INC_DIR), sl_queue.h) $(SL_LIST_INC)
+DQ_QUEUE_INC := $(addprefix $(LIB_DIR), queues/dq_queue.c) $(addprefix $(INC_DIR), dq_queue.h) $(DL_DEQUE_INC)
 
 # Build target for singly-list-backed queue
 $(addprefix $(BUILD_DIR), sl_queue.partial): $(SL_QUEUE_INC) | build
@@ -9,8 +10,8 @@ $(addprefix $(BUILD_DIR), sl_queue.o): $(addprefix $(BUILD_DIR), sl_queue.partia
 
 sl_queue.o: $(addprefix $(BUILD_DIR), sl_queue.o)
 
-# Build target for singly-list-backed queue
-$(addprefix $(BUILD_DIR), dq_queue.partial): $(addprefix $(LIB_DIR), queues/dq_queue.c) $(addprefix $(INC_DIR), dq_queue.h dl_deque.h) | build
+# Build target for deque-backed queue
+$(addprefix $(BUILD_DIR), dq_queue.partial): $(DQ_QUEUE_INC) | build
 	$(CC) $(CFLAGS) -c $< -o $@
 
 $(addprefix $(BUILD_DIR), dq_queue.o): $(addprefix $(BUILD_DIR), dq_queue.partial dl_deque.o)
--- lib/stacks/build.mk
+++ lib/stacks/build.mk
@@ -1,4 +1,5 @@
 SL_STACK_INC := $(addprefix $(LIB_DIR), stacks/sl_stack.c) $(addprefix $(INC_DIR), sl_stack.h) $(SL_LIST_INC)
+DQ_STACK_INC := $(addprefix $(LIB_DIR), stacks/dq_stack.c) $(addprefix $(INC_DIR), dq_stack.h) $(DL_DEQUE_INC)
 
 # Build target for singly-list-backed stack
 $(addprefix $(BUILD_DIR), sl_stack.partial): $(SL_STACK_INC) | build
@@ -10,7 +11,7 @@ $(addprefix $(BUILD_DIR), sl_stack.o): $(addprefix $(BUILD_DIR), sl_stack.partia
 sl_stack.o: $(addprefix $(BUILD_DIR), sl_stack.o)
 
 # Build target for deque-backed stack
-$(addprefix $(BUILD_DIR), dq_stack.partial): $(addprefix $(LIB_DIR), stacks/dq_stack.c) $(addprefix $(INC_DIR), dq_stack.h dl_deque.h) | build
+$(addprefix $(BUILD_DIR), dq_stack.partial): $(DQ_STACK_INC) | build
 	$(CC) $(CFLAGS) -c $< -o $@
 
 $(addprefix $(BUILD_DIR), dq_stack.o): $(addprefix $(BUILD_DIR), dq_stack.partial dl_deque.o)
