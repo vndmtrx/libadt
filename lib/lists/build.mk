7f3f6cc9bb5b449018ce0fdfb0c2bffb024660dd
Eduardo Rolim <eduardo.rolim@ifpr.edu.br>
2016-04-07 08:39:25 -0300

    Add build target for Circular List lib.

--- lib/lists/build.mk
+++ lib/lists/build.mk
@@ -1,5 +1,6 @@
 SL_LIST_INC := $(addprefix $(LIB_DIR), lists/sl_list.c) $(addprefix $(INC_DIR), sl_list.h)
 DL_LIST_INC := $(addprefix $(LIB_DIR), lists/dl_list.c) $(addprefix $(INC_DIR), dl_list.h)
+CL_LIST_INC := $(addprefix $(LIB_DIR), lists/cl_list.c) $(addprefix $(INC_DIR), cl_list.h)
 
 # Build target for Single Linked List
 $(addprefix $(BUILD_DIR), sl_list.o): $(SL_LIST_INC) | build
@@ -12,3 +13,9 @@ $(addprefix $(BUILD_DIR), dl_list.o): $(DL_LIST_INC) | build
 	$(CC) $(CFLAGS) -c $< -o $@
 
 dl_list.o: $(addprefix $(BUILD_DIR), dl_list.o)
+
+# Build target for Circular Linked List
+$(addprefix $(BUILD_DIR), cl_list.o): $(CL_LIST_INC) | build
+	$(CC) $(CFLAGS) -c $< -o $@
+
+dl_list.o: $(addprefix $(BUILD_DIR), cl_list.o)
