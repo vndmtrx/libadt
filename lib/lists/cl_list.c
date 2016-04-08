fe3da9e42d329f9f0d942d8f75fd866c363d7d33
Eduardo Rolim <eduardo.rolim@ifpr.edu.br>
2016-04-08 11:40:54 -0300

    Add insert functions on cl_list.c.

--- lib/lists/cl_list.c
+++ lib/lists/cl_list.c
@@ -13,12 +13,62 @@ cl_list_root * cl_list_create(t_destroyfunc destroyfunc) {
 }
 
 int cl_list_insert_el_next(cl_list_root *list, list_node *current, void *data) {
-	
+	list_node *new = (list_node *) malloc(sizeof(list_node));
+	if (new == NULL) {
+		fprintf(stderr, "Can't create new element.");
+		return -1;
+	}
+	new->data = data;
+	new->prev = NULL;
+	new->next = NULL;
+	if (list_size(list) == 0) {
+		list->head = new;
+		new->next = new;
+		new->prev = new;
+	} else {
+		if (current == NULL) {
+			new->next = list->head;
+			new->prev = list->head->prev;
+			list->head->prev = new;
+			list->head = new;
+		} else {
+			new->next = current->next;
+			new->prev = current;
+			current->next->prev = new;
+			current->next = new;
+		}
+	}
+	list->size++;
 	return 0;
 }
 
 int cl_list_insert_el_prev(cl_list_root *list, list_node *current, void *data) {
-	
+	list_node *new = (list_node *) malloc(sizeof(list_node));
+	if (new == NULL) {
+		fprintf(stderr, "Can't create new element.");
+		return -1;
+	}
+	new->data = data;
+	new->prev = NULL;
+	new->next = NULL;
+	if (list_size(list) == 0) {
+		list->head = new;
+		new->next = new;
+		new->prev = new;
+	} else {
+		if (current == NULL) {
+			new->prev = list->head;
+			new->next = list->head->next;
+			list->head->next = new;
+			list->head = new;
+		} else {
+			new->prev = current->prev;
+			new->next = current;
+			current->prev->next = new;
+			current->prev = new;
+		}
+	}
+	list->size++;
 	return 0;
 }
 
