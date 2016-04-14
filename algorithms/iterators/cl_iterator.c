a1bbd6a74288bbfb38cb1b74cb447a7616aec059
Eduardo Rolim <eduardo.rolim@ifpr.edu.br>
2016-04-14 15:57:25 -0300

    Add implementation code to cl_iterator.c.

--- algorithms/iterators/cl_iterator.c
+++ algorithms/iterators/cl_iterator.c
@@ -4,3 +4,60 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+
+struct iterator_c {
+	list_node *current;
+	cl_iter_direction flow;
+};
+
+iterator_c * cl_iter_create(list_root *list, dl_iter_direction d) {
+	iterator_c *iter = NULL;
+	if (list->head != NULL) {
+		iter = (iterator_c *) malloc(sizeof(iterator_c));
+		iter->flow = d;
+		switch (iter->flow) {
+			case FORWARD:
+			case BACKWARD: iter->current = list->head; break;
+			default: free(iter); return NULL;
+		}
+	}
+	return iter;
+}
+
+int cl_iter_hasnext(iterator_c *iter) {
+	if (iter != NULL) {
+		return 1;
+	} else {
+		retuen 0;
+	}
+}
+
+int cl_iter_next(iterator_c *iter) {
+	if (iter != NULL) {
+		if (iter->current != NULL) {
+			switch (iter->flow) {
+				case FORWARD: iter->current = iter->current->next; break;
+				case BACKWARD: iter->current = iter->current->prev; break;
+				default: return 0;
+			}
+		}
+		return (iter->current != NULL);
+	}
+	return 0;
+}
+
+void * cl_iter_item(iterator_c *iter) {
+	if (iter != NULL) {
+		if (iter->current != NULL) {
+			return iter->current->data;
+		}
+	}
+	return NULL;
+}
+
+void cl_iter_free(iterator_c *iter) {
+	if (iter != NULL) {
+		iter->current = NULL;
+		free(iter);
+	}
+}
