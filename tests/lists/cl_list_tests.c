fca3fe6b3b8f7a918025c81597fe7e4c7e450887
Eduardo Rolim <eduardo.rolim@ifpr.edu.br>
2016-04-14 16:23:56 -0300

    Add list_iter for circular list test.

--- tests/lists/cl_list_tests.c
+++ tests/lists/cl_list_tests.c
@@ -5,3 +5,20 @@
 #include <cl_iterator.h>
 
 #define QTD 100
+
+void print_list_iter(cl_list_root *list) {
+	iterator_c *i = cl_iter_create(list, FORWARD);
+	printf("[");
+	if (i != NULL) {
+		do {
+			printf("'%d', ", *((int *) cl_iter_item(i)));
+		} while (cl_iter_next(i) && cl_iter_is_item(i, list->head));
+	};
+	printf("]\n");
+	cl_iter_free(i);
+}
+
+int main() {
+	
+	return EXIT_SUCCESS;
+}
