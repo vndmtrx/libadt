abd17d06d7b0a1bbedb597b9f00b51836fcd1ba0
Eduardo Rolim <eduardo.rolim@ifpr.edu.br>
2016-04-14 15:43:57 -0300

    Fix tests which uses dl_iterator.h.

--- tests/deques/dl_deque_tests.c
+++ tests/deques/dl_deque_tests.c
@@ -7,7 +7,7 @@
 #define QTD 100
 
 void print_deque_iter(dl_deque_root *deque) {
-	iterator_s *i = dl_iter_create(deque, FORWARD);
+	iterator_d *i = dl_iter_create(deque, FORWARD);
 	printf("[");
 	if (i != NULL) {
 		do {
