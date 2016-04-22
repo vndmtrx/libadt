adf7d532a3b0750ef015b81e4085d56040e18921
Eduardo Rolim <vndmtrx@gmail.com>
2016-04-22 13:59:44 -0300

    Add build object variables to build targets.

--- lib/deques/build.mk
+++ lib/deques/build.mk
@@ -1,11 +1,13 @@
 DL_DEQUE_INC := $(addprefix $(LIB_DIR), deques/dl_deque.c) $(addprefix $(INC_DIR), dl_deque.h) $(DL_LIST_INC)
 
+DL_DEQUE_OBJ := $(addprefix $(BUILD_DIR), dl_deque.o) $(DL_LIST_OBJ) 
+
 # Build target for doubly-list-backed deque
 
 $(addprefix $(BUILD_DIR), dl_deque.o): $(DL_DEQUE_INC) | build
 	$(CC) $(CLIBFLAGS) -c $< -o $@
 
-$(addprefix $(BUILD_DIR), dl_deque.a): $(addprefix $(BUILD_DIR), dl_deque.o dl_list.a)
+$(addprefix $(BUILD_DIR), dl_deque.a): $(DL_DEQUE_OBJ)
 	$(AR) $(ARFLAGS) $@ $^
 	$(RL) $@
 
--- lib/lists/build.mk
+++ lib/lists/build.mk
@@ -2,12 +2,16 @@ SL_LIST_INC := $(addprefix $(LIB_DIR), lists/sl_list.c) $(addprefix $(INC_DIR), 
 DL_LIST_INC := $(addprefix $(LIB_DIR), lists/dl_list.c) $(addprefix $(INC_DIR), dl_list.h)
 CL_LIST_INC := $(addprefix $(LIB_DIR), lists/cl_list.c) $(addprefix $(INC_DIR), cl_list.h)
 
+SL_LIST_OBJ := $(addprefix $(BUILD_DIR), sl_list.o)
+DL_LIST_OBJ := $(addprefix $(BUILD_DIR), dl_list.o)
+CL_LIST_OBJ := $(addprefix $(BUILD_DIR), cl_list.o)
+
 # Build target for Single Linked List
 
 $(addprefix $(BUILD_DIR), sl_list.o): $(SL_LIST_INC) | build
 	$(CC) $(CLIBFLAGS) -c $< -o $@
 
-$(addprefix $(BUILD_DIR), sl_list.a): $(addprefix $(BUILD_DIR), sl_list.o)
+$(addprefix $(BUILD_DIR), sl_list.a): $(SL_LIST_OBJ)
 	$(AR) $(ARFLAGS) $@ $^
 	$(RL) $@
 
@@ -16,7 +20,7 @@ $(addprefix $(BUILD_DIR), sl_list.a): $(addprefix $(BUILD_DIR), sl_list.o)
 $(addprefix $(BUILD_DIR), dl_list.o): $(DL_LIST_INC) | build
 	$(CC) $(CLIBFLAGS) -c $< -o $@
 
-$(addprefix $(BUILD_DIR), dl_list.a): $(addprefix $(BUILD_DIR), dl_list.o)
+$(addprefix $(BUILD_DIR), dl_list.a): $(DL_LIST_OBJ)
 	$(AR) $(ARFLAGS) $@ $^
 	$(RL) $@
 
@@ -25,7 +29,7 @@ $(addprefix $(BUILD_DIR), dl_list.a): $(addprefix $(BUILD_DIR), dl_list.o)
 $(addprefix $(BUILD_DIR), cl_list.o): $(CL_LIST_INC) | build
 	$(CC) $(CLIBFLAGS) -c $< -o $@
 
-$(addprefix $(BUILD_DIR), cl_list.a): $(addprefix $(BUILD_DIR), cl_list.o)
+$(addprefix $(BUILD_DIR), cl_list.a): $(CL_LIST_OBJ)
 	$(AR) $(ARFLAGS) $@ $^
 	$(RL) $@
 
