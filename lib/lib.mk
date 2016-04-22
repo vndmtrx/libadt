31b2796783b1216444fa96af58f581e7f724dba9
Eduardo Rolim <vndmtrx@gmail.com>
2016-04-22 14:00:34 -0300

    Change lib target include order.
    The build variables doesn't function properly because a out of order include of deque build targets. Fixed.

--- lib/lib.mk
+++ lib/lib.mk
@@ -3,13 +3,13 @@
 #LISTS - Build target for Linked Lists
 include $(addprefix $(LIB_DIR), lists/build.mk)
 
+#DEQUES - Build target for Deques
+include $(addprefix $(LIB_DIR), deques/build.mk)
+
 #QUEUES - Build target for Queues
 include $(addprefix $(LIB_DIR), queues/build.mk)
 
 #STACKS - Build target for Stacks
 include $(addprefix $(LIB_DIR), stacks/build.mk)
-
-#DEQUES - Build target for Deques
-include $(addprefix $(LIB_DIR), deques/build.mk)
 
 libs: lists deques stacks queues
