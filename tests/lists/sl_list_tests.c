923cfed583585add8498b6ec67a9e150f99064d2
Eduardo Rolim <vndmtrx@gmail.com>
2018-03-22 20:22:43 -0300

    Fix function naming

--- tests/lists/sl_list_tests.c
+++ tests/lists/sl_list_tests.c
@@ -49,21 +49,21 @@ int main() {
 	print_list_iter(a);
 
 	printf("##### Test 4 - sl_list_move_el - Move head item to middle of the list\n");
-	sl_list_move_el(a, a->head, sl_list_next(sl_list_next(a->head)));
+	sl_list_move_el_next(a, a->head, sl_list_next(sl_list_next(a->head)));
 	print_list_iter(a);
 
 	printf("#####  Test 5 - sl_list_move_el - Move item to tail of the list\n");
-	sl_list_move_el(a, sl_list_next(a->head), a->tail);
+	sl_list_move_el_next(a, sl_list_next(a->head), a->tail);
 	print_list_iter(a);
 
 	printf("##### Test 6 - sl_list_move_el - Move item to another position\n");
-	sl_list_move_el(a, sl_list_next(sl_list_next(sl_list_next(a->head))), sl_list_next(a->head));
+	sl_list_move_el_next(a, sl_list_next(sl_list_next(sl_list_next(a->head))), sl_list_next(a->head));
 	print_list_iter(a);
 
 	printf("##### Test 7 - sl_list_move_el - Move head to tail (and back again)\n");
-	sl_list_move_el(a, a->head, a->tail);
+	sl_list_move_el_next(a, a->head, a->tail);
 	print_list_iter(a);
-	sl_list_move_el(a, a->tail, a->head);
+	sl_list_move_el_next(a, a->tail, a->head);
 	print_list_iter(a);
 
 	printf("##### Test 8 - sl_list_swap_el - Swap two items on the list\n");
